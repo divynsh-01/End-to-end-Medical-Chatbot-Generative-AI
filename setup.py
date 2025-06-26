17e3805d4ad4115ac8d8e6f66fcf2439776231fc
divynsh-01 <sdivyanshh958@gmail.com>
2025-06-26 15:12:01 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medibot',  # or 'askmike' when you switch later
+    version='0.1',
+    author='Divyansh Sharma',
+    author_email='sdivyanshh958@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
